307a097489e23bc54140d9f5e0ee26e7b726e116
Haydar Bekmuradov <haydarbekmuradov50@gmail.com>
2022-05-18 20:19:36 +0300

    issue-2: some fix everything is working

--- Grades iOS developers/LevelsVC/Intern/InternVC/InternViewController.swift
+++ Grades iOS developers/LevelsVC/Intern/InternVC/InternViewController.swift
@@ -9,9 +9,59 @@ import UIKit
 
 class InternViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    let computerScience = ["Computer Science:", "   Data", "   OOP", "   Architecture", "   Network"]
+    let iOSPlatform = ["iOS Platform:", "   iOS SDK + 3rd", "   UI", "   Threading", "   Tools", "   Xcode"]
+    let memoryManagement = ["Memory Management:", "   Memory"]
+    let language = ["Language:", "   Swift"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+    }
+}
 
-        // Do any additional setup after loading the view.
+extension InternViewController : UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "InternTableViewCell", for: indexPath) as! InternTableViewCell
+        
+        if indexPath.section == 0 {
+            cell.internTextLabel.text = computerScience[indexPath.row]
+        }
+        
+        if indexPath.section == 1 {
+            cell.internTextLabel.text = iOSPlatform[indexPath.row]
+        }
+        
+        if indexPath.section == 2 {
+            cell.internTextLabel.text = memoryManagement[indexPath.row]
+        }
+        
+        if indexPath.section == 3  {
+            cell.internTextLabel.text = language[indexPath.row]
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return computerScience.count
+        } else if section == 1 {
+            return iOSPlatform.count
+        } else {
+            return language.count
+        }
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 4
     }
 }
--- Grades iOS developers/MainVC/ViewController.swift
+++ Grades iOS developers/MainVC/ViewController.swift
@@ -9,18 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var tableVIew: UITableView!
-    
+    @IBOutlet weak var tableView: UITableView!
     let levels = ["Intern", "Junior 1", "Junior 2", "Junior 3", "Middle 1", "Middle 2", "Middle 3", "Senior"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableVIew.delegate = self
-        tableVIew.dataSource = self
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 }
 
 extension ViewController : UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: false)
+    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return levels.count
@@ -33,11 +36,9 @@ extension ViewController : UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
         if indexPath.section == 0 && indexPath.row == 0 {
             let vc1 = UIStoryboard(name: "InternVC", bundle: nil).instantiateViewController(withIdentifier: "InternViewController") as! InternViewController
             navigationController?.pushViewController(vc1, animated: true)
         }
-
     }
 }
